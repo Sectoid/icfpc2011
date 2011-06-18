9cf702000c188a00655581c8fd46c3f9709d26cb
Paul <psujkov@gmail.com>
2011-06-18 20:13:43 +0300

    Mutable array storage added

--- src/skiparser.hs
+++ src/skiparser.hs
@@ -1,6 +1,22 @@
 module SkiParser
        where
 
+import qualified Data.Sequence as S
+
+import Control.Monad.ST
+import Data.Array.ST
+
+import Types
+
+cells :: ST s (STArray s Int (Int, Value))
+cells = newArray (0, 255) (10000, CardValue I)
+
+update = do arr <- cells
+            a <- readArray arr 1
+            writeArray arr 1 (5000, CardValue I)
+            b <- readArray arr 1
+            return (a, b)
+
 data Function a b c d = Value   a
                       | Unary   (a -> b)
                       | Binary  (a -> b -> c)
@@ -22,7 +38,9 @@ succ = (+1)
 dbl :: Integer -> Integer
 dbl = (*2)
 
--- get
+-- State
+get :: Int -> (a -> a)
+get _ = id
 
 put :: a -> (a -> a)
 put _ = i
@@ -43,4 +61,4 @@ k = const
 
 
 main :: IO ()
-main = print ""
+main = print $ runST update

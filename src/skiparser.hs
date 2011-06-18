ea365a84075681e7dc109a8cedca55af54c4de5e
Paul <psujkov@gmail.com>
2011-06-18 21:25:58 +0300

    STArray updates for some actions

--- src/skiparser.hs
+++ src/skiparser.hs
@@ -7,9 +7,12 @@ import Control.Monad.ST
 import Data.Array.ST
 
 import Types
+import Interaction
 
-cells :: ST s (STArray s Int (Int, Value))
-cells = newArray (0, 255) (10000, CardValue I)
+makeArray :: Int -> Int -> a -> ST s (STArray s Int a)
+makeArray lower upper val = newArray (lower, upper) val
+
+cells = makeArray 0 255 (10000, CardValue I)
 
 update = do arr <- cells
             a <- readArray arr 1
@@ -39,8 +42,10 @@ dbl :: Integer -> Integer
 dbl = (*2)
 
 -- State
-get :: Int -> (a -> a)
-get _ = id
+get :: Int -> Value
+get n = snd $ runST ( do arr <- cells
+                         readArray arr n
+                    )
 
 put :: a -> (a -> a)
 put _ = i
@@ -51,12 +56,35 @@ s f g x = f x (g x)
 k :: a -> b -> a
 k = const
 
--- inc
--- dec
+inc :: Int -> (a -> a)
+inc n = runST ( do arr <- cells
+                   (vit, val) <- readArray arr n
+                   writeArray arr n (vit + 1, val)
+                   return i
+              )
+
+dec :: Int -> (a -> a)
+dec n = runST ( do arr <- cells
+                   (vit, val) <- readArray arr n
+                   writeArray arr n (vit - 1, val)
+                   return i
+              )
 -- attack
 -- help
 -- copy
--- revive
+
+revive' :: Int -> Int
+revive' n | n <= 0    = 1
+          | otherwise = n
+
+revive :: Int -> (a -> a)
+revive n = runST ( do arr <- cells
+                      (vit, val) <- readArray arr n
+                      writeArray arr n (revive' vit, val)
+                      return i
+              )
+
+
 -- zombie
 
 

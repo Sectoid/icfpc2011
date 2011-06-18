86cf39eccaa40f7ab8c452bd06e17e0cb5d56c99
Gleb Golubitsky <sectoid@gnolltech.org>
2011-06-18 19:04:10 +0300

    [LTG.Interaction] Some simple functions to create values in slots
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- src/Interaction.hs
+++ src/Interaction.hs
@@ -2,7 +2,21 @@ module LTG.Interaction where
 
 import LTG.Types
 
-data Direction = Left
-               | Right
+data Direction = DLeft
+               | DRight
 
 data Turn = Turn Direction Integer Card
+
+-- Returns squence of turns required to create value in certain slot
+create :: Value -> Integer -> [Turn]
+create (IntValue value) slot = createNumber value slot
+create (CardValue value) slot = resetSlot slot ++ [Turn DRight slot value]
+create _ _ = undefined
+
+-- TBD. Creates numeric value in specified slot
+createNumber :: Integer -> Integer -> [Turn]
+createNumber = undefined
+
+-- Resets slot value
+resetSlot :: Integer -> [Turn]
+resetSlot slotNumber = [Turn DLeft slotNumber Put]

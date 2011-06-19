d84ac1b662dff43cbbb22fb5ca4f235791929b74
Gleb Golubitsky <sectoid@gnolltech.org>
2011-06-20 01:19:02 +0300

    fixing Show instance
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- src/Types.hs
+++ src/Types.hs
@@ -15,9 +15,26 @@ data Card = I
           | Copy
           | Revive
           | Zombie
-          deriving (Eq, Show)
+          deriving (Eq)
 
 data Value = IntValue Integer
            | CardValue Card
            | AppValue Value Value
            deriving (Eq, Show)
+
+instance Show Card where 
+  show I = "I"
+  show S = "S"
+  show K = "K"
+  show Zero = "zero"
+  show Succ = "succ"
+  show Dbl = "dbl"
+  show Get = "get"
+  show Put = "put"
+  show Inc = "inc"
+  show Dec = "dec"
+  show Attack = "attack"
+  show Help = "help"
+  show Copy = "copy"
+  show Revive = "revive"
+  show Zombie = "zombie"
